adb1ff61730c94c001a9c70e65a4619bbcae603e
SPK80 <psukonkin@gmail.com>
2022-02-19 15:06:37 +0300

    refactor: content

--- content.js
+++ content.js
@@ -1,32 +1,42 @@
 //works when window loaded
+// console.log('PopupTweaker');
 window.onload = () => {
 	//if window is not fullscreen
 	if (window.outerWidth < screen.availWidth && window.outerHeight < screen.availHeight) {
-		//tune sizes by DOM html - main element size
-		var wi = document.getElementsByTagName('html')[0].scrollWidth * 1.3 + 50;
-		var he = document.getElementsByTagName('html')[0].scrollHeight * 1.3 + 50;
-		//resize window
-		// console.log('PopupTweaker resize window');
-		window.resizeTo(wi, he);
+		// console.log('loaded');
+		adjustWindow();
+		focusFirstTextarea();
+		focusFirstInput();
+	}
+}
 
-		var tas = document.getElementsByTagName("textarea");
-		if (tas.length > 0) {
-			// console.log('PopupTweaker focus textarea');
-			tas[0].focus();
-		}
+function adjustWindow() {
+	//tune sizes by DOM html - main element size
+	const wi = document.getElementsByTagName('html')[0].scrollWidth * 1.3 + 50;
+	const he = document.getElementsByTagName('html')[0].scrollHeight * 1.3 + 50;
+	window.resizeTo(wi, he);
+}
 
-		var tas = document.getElementsByTagName("input");
-		if (tas.length > 0) {
-			var i = 0;
-			while (i < tas.length) {
-				var type = tas[i].getAttribute("type");
-				if (type == "text" && !tas[i].hasAttribute("disabled")) {
-					// console.log('PopupTweaker focus input');
-					tas[i].focus();
-					break;
-				}
-				i++;
+function focusFirstTextarea() {
+	//try to focus on first textarea
+	const tags = document.getElementsByTagName("textarea");
+	if (tags.length > 0) {
+		tags[0].focus();
+	}
+}
+
+function focusFirstInput() {
+	//try to focus on first input
+	const tags = document.getElementsByTagName("input");
+	if (tags.length > 0) {
+		var i = 0;
+		while (i < tags.length) {
+			var type = tags[i].getAttribute("type");
+			if (type == "text" && !tags[i].hasAttribute("disabled")) {
+				tags[i].focus();
+				break;
 			}
+			i++;
 		}
 	}
 }
